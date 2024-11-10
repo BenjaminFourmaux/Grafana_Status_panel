9a50f8e0600142c00387cbda2fd126d4e1cc8a0f
BenjaminFourmaux <benjamin.fourmauxb@gmail.com>
2024-11-10 15:25:52 +0100

    Change demo dashboard according to fetures added

--- tests/panel.spec.ts
+++ tests/panel.spec.ts
@@ -1,7 +1,7 @@
 import { expect, test } from '@grafana/plugin-e2e';
 
 test('should display Base severity', async ({ gotoPanelEditPage, readProvisionedDashboard }) => {
-  const dashboard = await readProvisionedDashboard({ fileName: 'dashboard.json' });
+  const dashboard = await readProvisionedDashboard({ fileName: 'demo-dashboard.json' });
   const panelEditPage = await gotoPanelEditPage({ dashboard, id: '2' });
   await expect(panelEditPage.panel.locator).toContainText('success');
 });
