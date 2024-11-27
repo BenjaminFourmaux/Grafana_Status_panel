5b72d5d19528b2b8fd4163dc1645da634c2a3aef
Benjamin Fourmaux <49273769+BenjaminFourmaux@users.noreply.github.com>
2024-11-27 20:01:12 +0100

    Beging to test Aggregation. block cause select option non reachable

---
+++ tests/AggregationMode.spec.ts
@@ -0,0 +1,24 @@
+import { expect, test } from '@grafana/plugin-e2e';
+// @ts-ignore
+import { Arrange, convertDateTime, GetPanelCardAttribute, SetPanelOption } from './utils';
+import { Locator } from '@playwright/test';
+
+/**
+ * Test scenario: Test Aggregation Mode Last
+ */
+test('Last', async ({ page, panelEditPage }) => {
+  const queries_value = '1,2,3,4,5,6,5,4,3,2,1';
+  const expected_value = '1';
+
+  // Arrange
+  await Arrange(page, panelEditPage, queries_value);
+
+  // Act
+  await SetPanelOption(page, 'StayOn', 'Front'); // Display side card
+  await SetPanelOption(page, 'Aggregation', 'Last');
+  await page.waitForTimeout(3000);
+
+  // Assert
+  let locator = (await GetPanelCardAttribute(page, 'Subtitle', 0)) as Locator;
+  expect(await locator.textContent()).toBe(expected_value);
+});
