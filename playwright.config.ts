630933a80749d6e3d040719fe995c0a4c40a62b0
Benjamin Fourmaux <49273769+BenjaminFourmaux@users.noreply.github.com>
2025-04-16 17:21:51 +0200

    Change the plugin FQDN after the plugin release on store (#10)
    * Change the plugin FQDN by the one of the release plugin on demo dachboard
    * Fix pipelines

--- playwright.config.ts
+++ playwright.config.ts
@@ -33,6 +33,11 @@ export default defineConfig<PluginOptions>({
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
 
+    /* Fix bug : https://github.com/grafana/plugin-tools/issues/1733 */
+    launchOptions: {
+      args: ['--disable-features=PlzDedicatedWorker'], // because https://github.com/microsoft/playwright/pull/34400
+    },
+
     /* Display the web browser UI. (true to hidden) */
     headless: true,
   },
